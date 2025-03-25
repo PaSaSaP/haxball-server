9cbabca4f5afdec835d3fbd48597f801ce9dbc56
haxball.ovh <wolta.hydrosfera_7d@icloud.com>
2025-03-25 21:18:50 +0100

    fix get_trust_history.sh

--- scripts/get_trust_history.sh
+++ scripts/get_trust_history.sh
@@ -4,12 +4,17 @@ DB_FILE="$1"
 AUTH_ID="$2"
 
 # Sprawdzamy, czy podano wszystkie argumenty
-if [[ -z "$DB_FILE" ]] || [[ -z "$AUTH_ID" ]]; then
-  echo "Usage: $0 <db_file> <auth_id>"
+if [[ -z "$DB_FILE" ]]; then
+  echo "Usage: $0 <db_file> [auth_id]"
   exit 1
+fi
+
+WHERE_CLAUSE=''
+if [[ -n $AUTH_ID ]]; then
+  WHERE_CLAUSE="WHERE auth_id='$AUTH_ID'"
 fi
 
 sqlite3 "$DB_FILE" <<EOF
   SELECT * FROM history_players_trust
-  WHERE auth_id = '$auth_id';
+  $WHERE_CLAUSE;
 EOF
